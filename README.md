dfbcaea4093db41c43aa3818a9bb164f288ced9a
Harikrushna V. Adiecha <adiechahari@gmail.com>
2016-08-03 12:41:43 +0530

    basic angular task done

---
+++ README.md
@@ -0,0 +1,9 @@
+Hello,
+
+here is the instructions to run this project.
+
+1. you need to simply download or clone it to your system
+
+2. put that folder into your web directory (www or htdocs as per your installation)
+
+3. and then simply open that project in the browser as localhost
---
+++ js/app.js
@@ -0,0 +1,45 @@
+angular.module('angular-app-assignment', [])
+
+.controller('pascalTriangleCtrl', ['$scope', function($scope) {
+
+  $scope.length = null;
+
+  $scope.shouldDisplayTriangle = function(l) {
+    return (l != null && l != 0);
+  }
+
+  $scope.getList = function(l) {
+    var arr = [];
+    for (var i = 0; i < l; i++) {
+      arr.push(i);
+    }
+    return arr;
+  }
+
+  $scope.getPascalValue = function(row, column) {
+    if(row < column) return "";
+    else if(row == column || column == 0) return 1;
+    else return ($scope.getPascalValue(row-1, column) + $scope.getPascalValue(row-1, column-1));
+  }
+
+
+  $scope.getClass = function(num) {
+    var cls = "";
+    switch(num % 4) {
+      case 0:
+        cls = "red-text";
+        break;
+      case 1:
+        cls = "blue-text";
+        break;
+      case 2:
+        cls = "orange-text";
+        break;
+      case 3:
+        cls = "green-text";
+        break;
+    }
+    return cls;
+  }
+
+}]);
